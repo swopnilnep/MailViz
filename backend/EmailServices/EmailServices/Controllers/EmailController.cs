34644eb0b6c3359b9b457d5cef683720f54d758b
Swopnil N. Shrestha <swopnilnep@gmail.com>
2019-06-10 14:46:37 -0400

    fix EntityFramework not found error

--- backend/EmailServices/EmailServices/Controllers/EmailController.cs
+++ backend/EmailServices/EmailServices/Controllers/EmailController.cs
@@ -4,10 +4,32 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EmailDataAccess;
 
 namespace EmailServices.Controllers
 {
     public class EmailController : ApiController
     {
+
+        // Get all email addresses (and related entires)
+        public IEnumerable< tbl_ex_EmailAddresses > Get()
+        {
+
+            using (EmailDetailsEntities entities = new EmailDetailsEntities())
+            {
+                return entities.tbl_ex_EmailAddresses.ToList();
+            }
+
+        }
+
+        // Get email address by id (and related entries)
+        public tbl_ex_EmailAddresses Get(int id)
+        {
+            using(EmailDetailsEntities entities = new EmailDetailsEntities)
+            {
+                return entities.tbl_ex_EmailAddresses.FirstOrDefault(e => e.EmailAddressID == id);
+            }
+        }
+
     }
 }
