d236cebe8556e26002cd6e8ff1d411bd897d6d75
Swopnil N. Shrestha <swopnilnep@gmail.com>
2019-06-17 17:31:07 -0400

    Add time API configurations

--- backend/EmailServicesAPI/EmailServicesAPI/Controllers/EmailController.cs
+++ backend/EmailServicesAPI/EmailServicesAPI/Controllers/EmailController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
 using static EmailServicesAPI.Context.DatabaseContext;
 
@@ -7,11 +8,8 @@ namespace EmailServicesAPI.Controllers
 {
     public class EmailController : ApiController
     {
-        private VenContext db = new VenContext(
-            System.Configuration
-            .ConfigurationManager
-            .ConnectionStrings["EmailDetailsEntities"].ConnectionString
-            );
+        private VenContext db = 
+            VenioWebApiHelper.getDatabaseContext();
 
         [HttpGet]
         public IHttpActionResult Get()
@@ -59,11 +57,8 @@ namespace EmailServicesAPI.Controllers
     public class PeopleController : ApiController
     {
 
-        private VenContext db = new VenContext(
-            System.Configuration
-            .ConfigurationManager
-            .ConnectionStrings["EmailDetailsEntities"].ConnectionString
-            );
+        private VenContext db = 
+             VenioWebApiHelper.getDatabaseContext();
 
         [HttpGet]
         public IHttpActionResult Get()
@@ -88,6 +83,82 @@ namespace EmailServicesAPI.Controllers
             }
         }
 
+    }
+
+    public class TimeController : ApiController
+    {
+        private VenContext db =
+            VenioWebApiHelper.getDatabaseContext();
+
+        [HttpGet]
+        public IHttpActionResult getDateTime(
+            [FromUri] DateTime start, [FromUri] DateTime end
+            )
+        {
+
+            if (start > end)
+                return Content(System.Net.HttpStatusCode.BadRequest,
+                    "End Date cannot be before Start Date");
+
+            try
+            {
+                // Selects the ID that does not have a name or an email
+                // address and stores it as an integer (64Bit Signed)
+                long unknownsID = Convert.ToInt64((
+                        from ead in db.EmailAddressList
+                        where ead.EmailName == "[NO_NAME]"
+                        && ead.EmailAddress == "[NO_ADDRESS]"
+                        select ead.Id
+                    ).ToList().ElementAt(0));
+
+                var query = db.EmailAddresses
+                    .Where(tbl =>
+                        tbl.Date <= end && tbl.Date >= start)
+                    .Select(tbl =>
+                       new
+                       {
+                           tbl.SenderID,
+                           tbl.RecepientID,
+                       }
+                    )
+                    .Where(tbl => 
+                        tbl.RecepientID != unknownsID && tbl.SenderID != unknownsID)
+                    .GroupBy(tbl => new { tbl.SenderID, tbl.RecepientID })
+                    .Select(tbl =>
+                       new
+                       {
+                           tbl.Key.SenderID,
+                           tbl.Key.RecepientID,
+                           EmailsSent = tbl.Count()
+                       }
+                    ).OrderByDescending(tbl => tbl.EmailsSent);
+
+                return Ok(query);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError();
+            }
+        }
+
+    }
+
+    public static class VenioWebApiHelper
+    {
+
+        //
+        // Public Accessors
+        //
+
+        public static VenContext getDatabaseContext()
+        {
+            return new VenContext(
+                System.Configuration
+                .ConfigurationManager
+                .ConnectionStrings["EmailDetailsEntities"]
+                .ConnectionString
+                );
+        }
     }
 
 }
