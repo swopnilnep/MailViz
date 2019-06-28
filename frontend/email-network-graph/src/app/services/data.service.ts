886981ce03cb365d57d2210db2c4e8e498a0113c
Swopnil N. Shrestha <swopnilnep@gmail.com>
2019-06-28 19:07:12 -0400

    Refactored code for data.service.ts

--- frontend/email-network-graph/src/app/services/data.service.ts
+++ frontend/email-network-graph/src/app/services/data.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  /**
+   * Purpose
+   *  The DataService is the service for storing
+   *  and accessing project data throughout the project.
+   *  Any component or service can simply import this 
+   *  service to access public data across the project
+   *  
+   *  The dataservice is also utilized for fetching 
+   *  from the API and potentially other DataSources
+   *  using the 'ApiService'
+   */
+
+
+  //
+  // Public Class Fields
+  //
+
+  private startDate : Date = new Date('2001-05-06');
+  private endDate : Date = new Date('2001-05-09');
+
+  //
+  // Public Accessors
+  //
+
+  public getStartDate( ){
+    return this.startDate;
+  }
+
+  public getEndDate( ){
+    return this.endDate;
+  }
+
+  //
+  // Public Mutators
+  //
+
+  public setStartDate( other : Date ){
+    this.startDate = other;
+  }
+
+  public setEndDate( other : Date ){
+    this.endDate = other;
+  }
+
+
+  //
+  // Constructors
+  //
+
+  constructor(api : ApiService) { }
 }
